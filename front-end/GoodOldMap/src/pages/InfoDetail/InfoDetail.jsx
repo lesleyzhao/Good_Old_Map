b82a3a4ce012cbe3f360819da114fd9677edacbe
Nina1o1 <qx523@nyu.edu>
2023-10-24 18:02:44 -0400

    info detail page set up

--- front-end/GoodOldMap/src/pages/InfoDetail/InfoDetail.jsx
+++ front-end/GoodOldMap/src/pages/InfoDetail/InfoDetail.jsx
@@ -6,8 +6,9 @@ const InfoDetail = () => {
   props.parag = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.  "
   return (
     <>
-      <div className="h-screen flex">
-        <div className="w-[80%] mt-[10%] mx-auto">
+    {/* TODO: add back button, forward button, image */}
+      <div className="min-h-screen flex">
+        <div className="w-[80%] my-[10%] mx-auto">
           <h1>{props.header}</h1>
           <div className="mt-2">
             {props?.subtitle?.map((subtitle, i) => <h3 key={i}>{subtitle}</h3>)}
