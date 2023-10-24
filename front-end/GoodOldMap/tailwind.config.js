cd86f5d70df4030a79ae32c88fb9b70d1838f5b9
Nina1o1 <qx523@nyu.edu>
2023-10-24 11:19:31 -0400

    add color palette

--- front-end/GoodOldMap/tailwind.config.js
+++ front-end/GoodOldMap/tailwind.config.js
@@ -7,7 +7,12 @@ export default {
   ],
 
   theme: {
-    extend: {},
+    extend: {
+      "beige-1":"#F8F0E5",
+      "beige-2":"#EADBC8",
+      "beige-3":"#DAC0A3",
+      "navy-blue":"#0F2C59",
+    },
   },
   plugins: [],
 }
