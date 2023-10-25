c23a067b5db295f24d094ac64ad7335c022bf45d
Nina1o1 <qx523@nyu.edu>
2023-10-25 03:29:22 -0400

    add img to info page

--- front-end/GoodOldMap/src/pages/InfoDetail/InfoDetail.jsx
+++ front-end/GoodOldMap/src/pages/InfoDetail/InfoDetail.jsx
@@ -1,12 +1,13 @@
 import NavBar from "../../components/common/navBar";
 import RightBtn from "../../components/common/rightBtn";
 const InfoDetail = () => {
-  // parameters: header, subtitle(string array), parag
+  // parameters: pic, header, subtitle(string array), parag
   let props = {};
+  props.pic = "https://upload.wikimedia.org/wikipedia/commons/thumb/7/75/Vincent_van_Gogh_-_Road_with_Cypress_and_Star_-_c._12-15_May_1890.jpg/1200px-Vincent_van_Gogh_-_Road_with_Cypress_and_Star_-_c._12-15_May_1890.jpg"
   props.header = "This Is a Header"
   props.subtitle = ["Author: some author", "Time: some time"]
   props.parag = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.  "
-  
+
   const handleRightClick = (evt) => {
     evt.stopPropagation();
   }
@@ -18,12 +19,13 @@ const InfoDetail = () => {
           <img className="w-11 h-11" src="/back.png" alt="back"/>
           <RightBtn handleRightClick={handleRightClick}/>
         </NavBar>
+        <img className="max-h-[80vh] max-w-full object-contain" src={props.pic} alt="picture" />
         <div className="w-[80%] mb-[10%] mx-auto">
-          <div className="mt-4">
-            <h1>{props.header}</h1>
+          <div className="mt-2">
+            <h2>{props.header}</h2>
           </div>
           <div className="mt-2">
-            {props?.subtitle?.map((subtitle, i) => <h3 key={i}>{subtitle}</h3>)}
+            {props?.subtitle?.map((subtitle, i) => <p key={i}>{subtitle}</p>)}
           </div>
           <div className="mt-4">
             <p>{props.parag}</p>
