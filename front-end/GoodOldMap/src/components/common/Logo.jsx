3ef4bc1038206d259d0eb5b0ef58e97538865150
Nina Xiao <116832984+Nina1o1@users.noreply.github.com>
2023-10-25 01:57:02 -0400

    del note

--- front-end/GoodOldMap/src/components/common/Logo.jsx
+++ front-end/GoodOldMap/src/components/common/Logo.jsx
@@ -7,7 +7,6 @@ const Logo = () => {
   }
   return (
     <>
-    {/* TODO: margin-top too large for desktop */}
     {/* Reference: <a href="https://www.flaticon.com/free-icons/map" title="map icons">Map icons created by Freepik - Flaticon</a> */}
     <img className="w-11 hover:cursor-pointer" src="/maplogo.png" alt="maplogo" onClick={handleClick}/>
     </>
