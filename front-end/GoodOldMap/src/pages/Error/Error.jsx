873fdf32ebaea86128ac2ec6cc57b5a9199d5907
Nina1o1 <qx523@nyu.edu>
2023-10-26 19:08:27 -0400

    error page display

--- front-end/GoodOldMap/src/pages/Error/Error.jsx
+++ front-end/GoodOldMap/src/pages/Error/Error.jsx
@@ -2,7 +2,7 @@ import PageLink from "../../components/common/pageLink"
 const Error = () => {
   return(
     <>
-      <h1 className="w-full">404 Not Found</h1>
+      <h1 className="w-full text-center">404 Not Found</h1>
       <PageLink to="/" value="Go back to main page"/>
     </>
   )
