2491e95b274f6c84116679a7e853aeb3f828326b
Nina1o1 <qx523@nyu.edu>
2023-10-26 00:26:44 -0400

    navigate feature of back btn

--- front-end/GoodOldMap/src/components/common/leftBtn.jsx
+++ front-end/GoodOldMap/src/components/common/leftBtn.jsx
@@ -1,7 +1,11 @@
+import { useLocation, useNavigate } from "react-router-dom";
 const LeftBtn = () => {
+  const location = useLocation();
+  const navifate = useNavigate();
+  // naviagte to previous page via state={from: pathName} property passed to the Link
   const handleClick = (evt) => {
     evt.stopPropagation();
-
+    navifate(location?.state?.from)
   }
   return (
     <>
--- front-end/GoodOldMap/src/pages/Error/Error.jsx
+++ front-end/GoodOldMap/src/pages/Error/Error.jsx
@@ -1,9 +1,12 @@
 import PageLink from "../../components/common/pageLink"
+import { useLocation } from "react-router-dom"
 const Error = () => {
+  const location = useLocation();
   return(
     <>
       <h1 className="w-full">404 Not Found</h1>
       <PageLink to="/" value="Go back to main page"/>
+      <PageLink to="/info" from={location.pathname} value="info page"/>
     </>
   )
 }
