566f9cd837c83efdf2af8adc7810c806b799395e
lunnnnnn <cl5515@nyu.edu>
2023-10-31 00:24:19 -0400

    Account Page

--- front-end/GoodOldMap/src/pages/Account/Account.jsx
+++ front-end/GoodOldMap/src/pages/Account/Account.jsx
@@ -1,5 +1,5 @@
 
-import React from "react"
+import React, { useState } from "react";
 import AccountLayout from "./AccountLayout";
 import { Link } from "react-router-dom";
 import PopupLink from '../../components/popup/popupLink';
@@ -7,35 +7,48 @@ import PopupLink from '../../components/popup/popupLink';
 const Account = () => {
   // parameters: pic, username, email,
   let props = {};
-  props.pic = "https://cdn.icon-icons.com/icons2/2645/PNG/512/person_icon_159921.png"
+  props.pic = "https://picsum.photos/200"
   props.username = "John Doe"
   props.email = "jd00001@nyu.edu"
-  
+
+  const [showPopup, setShowPopup] = useState(false);
+
+  const togglePopup = () => {
+    setShowPopup(!showPopup);
+  }
+
+  const randomPhoto = "https://picsum.photos/500";
+
   return(
-    <div>    
-      <div className="min-h-screen flex flex-col items-center justify-center">
-        <Link to="changeProfilePic">
+    <div className="flex items-center justify-center h-screen flex-col">
+      <div className="bg-white rounded-lg shadow-lg p-8 flex items-center">
+        <div onClick={togglePopup}>
           <img className="w-32 h-32 rounded-full object-cover cursor-pointer" src={props.pic} alt="profile picture" />
+        </div>
+        {showPopup && (
+          <div className="fixed top-0 left-0 w-full h-full bg-gray-500 bg-opacity-50 flex items-center justify-center z-10" onClick={togglePopup}>
+            <div className="bg-white rounded-lg shadow-lg p-8 flex items-center">
+              <img className="w-64 h-64 rounded-full object-cover cursor-pointer" src={props.pic} alt="profile picture" />
+            </div>
+          </div>
+        )}
+        <div className="ml-8">
+          <h1 className="text-2xl font-bold">{props.username}</h1>
+          <p className="text-gray-500">{props.email}</p>
+          <Link to="edit">
+            <button className="bg-white-500 text-black font-bold py-2 px-4 rounded mt-4">
+              Edit Account
+            </button>
           </Link>
-
-        <h1 className="text-2xl font-bold mt-4">{props.username}</h1>
-        <p className="text-gray-500">{props.email}</p>
-        {/* Add a button that redirects to another place */}
-        <Link to="edit">
-          <button className="bg-white-500 text-black font-bold py-2 px-4 rounded mt-4">
-          Edit Account
-          </button>
-        </Link>
-
-        <Link to="/favoritelist">
-
-          <button className="bg-white-500 text-black font-bold py-2 px-4 rounded mt-4">
-          My Fav
-          </button>
-        </Link>
+        </div>
       </div>
-      
+      <h2 className="text-left"> My Favorite</h2>
+
+      <a href="/favoritelist" className="bg-white rounded-lg shadow-lg p-8 flex items-center mt-8">
+        <img className="w-64 h-64 object-cover cursor-pointer" src="https://picsum.photos/500" alt="random photo" />
+      </a>
     </div>
   )
 }
-export default Account
+
+export default Account;
