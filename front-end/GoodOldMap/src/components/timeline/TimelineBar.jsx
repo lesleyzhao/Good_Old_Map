3431dc55d1d0877e82171d7d2fe97e6c36c0a2c5
Charlotte <charlotte.ma0728@gmail.com>
2023-11-01 11:05:57 -0400

    edit main map

--- front-end/GoodOldMap/src/components/timeline/TimelineBar.jsx
+++ front-end/GoodOldMap/src/components/timeline/TimelineBar.jsx
@@ -1,28 +1,36 @@
+import { useState, useEffect } from 'react';
 import Draggable from 'react-draggable';
-import { useState } from 'react';
 
 function TimelineBar() {
   const [xPos, setXPos] = useState(0);
   const [year, setYear] = useState(2023);
+  const [parentWidth, setParentWidth] = useState(0);
+
+  useEffect(() => {
+    const audioProgress = document.getElementById('audio-progress');
+    if (audioProgress) {
+      setParentWidth(audioProgress.clientWidth);
+    }
+  }, []);
 
   const handleDrag = (e, ui) => {
-    const parentWidth = e.target.parentNode.parentNode.clientWidth;
-    const left = ui.x + 40;
-    const xPosPercent = (left / parentWidth) * 100;
+    const left = ui.x;
+    let xPosPercent = (left / parentWidth) * 100;
+    xPosPercent = Math.min(100, Math.max(0, xPosPercent));
     setXPos(xPosPercent);
     document.getElementById('audio-progress-bar').style.width = `${xPosPercent}%`;
-    console.log(left);
-    
-    const calculatedYear = Math.ceil((2023 * (left / parentWidth)));
-    setYear(calculatedYear); 
+    const calculatedYear = Math.round(2023 * (xPosPercent / 100));
+    setYear(calculatedYear);
+    console.log("left",left);
+    console.log(xPosPercent);
   };
 
   return (
-    <div id="audio-player-container" className='ml-10'>
+    <div id="audio-player-container" className="">
       <div className="h-audio_progress w-audio_progress bg-pregress rounded-md" id="audio-progress">
-        <Draggable axis="x" onDrag={handleDrag} defaultPosition={{ x: xPos, y: 0 }} bounds="parent">
-          <div id="draggable-point" className="float-left mt-[2px] mb-[5px]">
-            <div id="audio-progress-handle" className="block absolute z-10 mt-[-5px] ml-[30px] w-[14px] h-[14px] border-4 border-timelinePointer transform rotate-45 rounded-full bg-white shadow-[0 1px 6px rgba(0, 0, 0, .2)] cursor-pointer"></div>
+        <Draggable axis="x" onDrag={handleDrag} defaultPosition={{ x: 0, y: 0 }} bounds={{ left: 0, right: parentWidth}}>
+          <div className="float-left mt-[2px] mb-[5px]">
+            <div id="audio-progress-handle" className="block relative z-10 mt-[-5px] w-[14px] h-[14px] border-4 border-timelinePointer transform rotate-45 rounded-full bg-white shadow-[0 1px 6px rgba(0, 0, 0, .2)] cursor-pointer"></div>
           </div>
         </Draggable>
         <div id="audio-progress-bar" className="h-audio_bar bg-timeline rounded-md" style={{ width: `${xPos}%` }}></div>
