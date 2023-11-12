d50119a0920286adf69e6bef051737981d254521
lunnnnnn <cl5515@nyu.edu>
2023-11-11 20:26:57 -0500

    basic emailreset and passwordreset

---
+++ back-end/src/routes/resetpassword.mjs
@@ -0,0 +1,32 @@
+import e from "express";
+
+let users = {
+    "1234": {id: "1234", username: "John Doe", password: "password123"}
+   }
+
+const resetpassword = async(req, res) =>{
+    const {userID, oldPassword, newPassword} = req.body;
+
+    try{
+        //const wasUpdate = await database.updateUsername(newUserId, newUserName)
+        //Simulate database update
+        if(users[userID]){
+            if (users[userID].password != oldPassword){
+                res.status(400).json({message: "Wrong password."});
+            }
+            else{
+                users[userID].password = newPassword;
+                res.status(200).json({message: "Succesfully update password", user: users[userID]});
+                console.log('Updated user data:', users[userID])
+            }
+        }
+        else{
+            res.status(400).json({message: "User not found."});
+        }
+    }
+    catch(error){
+        res.status(500).json({message: "Server error occurred", error})
+    }
+
+}
+export default resetpassword;
