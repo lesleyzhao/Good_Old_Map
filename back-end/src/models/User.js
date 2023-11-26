8455e55b1fa88348e7499f94a2d1891a08252f12
lunnnnnn <cl5515@nyu.edu>
2023-11-25 22:15:38 -0500

    User Model

---
+++ back-end/src/models/User.js
@@ -0,0 +1,30 @@
+const moogoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const UserSchema = new moogoose.Schema({
+    uuid: {
+        type: String,
+        unique: true,
+        required: true,
+    },
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        lowercase: true,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+        select: false, // hide password by default
+    },
+    favorites: {
+        type: Array,
+        required: false,
+    },
+});
+
+const User = moogoose.model('User', UserSchema);
