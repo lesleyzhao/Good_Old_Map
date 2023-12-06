12d117ef5d9011ad048f660a699305f0da4913b7
Charlo-M <charlotte.ma0728@gmail.com>
2023-12-06 13:06:56 -0500

    delete forgotpassword from logged in users and update its version on login page

--- front-end/src/pages/Account/popupContent.jsx
+++ front-end/src/pages/Account/popupContent.jsx
@@ -2,34 +2,33 @@ import { FormInputsPopup } from "../../components/form/formInput";
 import { FormBtns } from "../../components/form/formBtn";
 import { forwardRef } from "react";
 
-
 const PopupContent = forwardRef((props, ref) => {
-  // props: title(str), inputs(array of object), buttons:(array of object)
-  // optional: message
+  // Function to stop propagation for clicks inside the popup content
+  const handleContentClick = (e) => {
+    e.stopPropagation(); // Prevents clicks within the content from closing the popup
+  };
+
   return (
-    // dark background
+    // Overlay: Closes the popup when clicked
     <div 
-      className="popupBackground fixed top-0 left-0 z-[1700] w-full h-full bg-black bg-opacity-50 flex items-center justify-center" 
+      className="popupBackground fixed top-0 left-0 z-[1700] w-full h-full bg-black bg-opacity-50 flex items-center justify-center"
       onClick={props.handleClick}>
-      {/* white popup container: */}
-      <div className="bg-white rounded-lg shadow-xl w-[80%] max-w-[30rem]">
-        
+      
+      {/* Content Area: Does not propagate clicks to the overlay */}
+      <div className="bg-white rounded-lg shadow-xl w-[80%] max-w-[30rem]" onClick={handleContentClick}>
         <div className="p-8 text-center">
-          {/* content area */}
           <h3 className="text-lg font-bold mb-4 text-center">{props?.title}</h3>
           <p>{props?.message}</p>
-          <div className="space-y-4" >
+          <div className="space-y-4">
             <FormInputsPopup inputs={props?.inputs}/>
-            <div className="flex flex-row gap-2 justify-end"> {/* Adjust button positioning as needed */}
+            <div className="flex flex-row gap-2 justify-end">
               <FormBtns buttons={props?.buttons}/>
             </div>
           </div>
         </div>
-
       </div>
     </div>
   );
 });
 
-
-export default PopupContent
+export default PopupContent;
--- front-end/src/pages/Authenticate/Login.jsx
+++ front-end/src/pages/Authenticate/Login.jsx
@@ -1,90 +1,132 @@
-import { useState } from 'react'
-import AuthHeader from './authHeader'
-import PageLink from '../../components/common/pageLink'
-import { FormInputs } from '../../components/form/formInput'
-import FormBtn from '../../components/form/formBtn'
-import { useNavigate } from 'react-router-dom'
-import { useRef } from 'react'
-import axiosProvider from '../../util/api/axios'
+import { useState, useRef } from 'react';
+import AuthHeader from './authHeader';
+import PopupContent from '../Account/popupContent';
+import PageLink from '../../components/common/pageLink';
+import { FormInputs } from '../../components/form/formInput';
+import FormBtn from '../../components/form/formBtn';
+import { useNavigate } from 'react-router-dom';
+import axiosProvider from '../../util/api/axios';
 
 const Login = () => {
-  const [message, setMessage] = useState("")
-  const fields = ["email", "password"]
+  const [loginMessage, setLoginMessage] = useState("");
+  const [popupMessage, setPopupMessage] = useState("");
+  const fields = ["email", "password"];
   const formRef = useRef(null);
+  const popupFormRef = useRef(null); 
+  const [currentActionData, setCurrentActionData] = useState(null);
   const navigate = useNavigate();
 
-  // click to login
-  const handleClick = async (evt) => {
-    evt.preventDefault();
+  const closePopup = () => {
+    setCurrentActionData(null);
+    setPopupMessage("");
+  };
 
-    const formData = new FormData(formRef.current)
-    const email = formData.get('email');
-    const password = formData.get('password');
-
-    const loginData = {
-      email,
-      password
-    };
-
-    const postOptions = {
-      headers: {
-        'Content-Type': 'application/json'
-      }
+  const handleLogin = async (evt) => {
+    evt.preventDefault(); // Prevents the default form submission behavior
+  
+    const formData = new FormData(formRef.current);
+    const email = formData.get('email'); // Extracts the email from the form data
+    const password = formData.get('password'); // Extracts the password from the form data
+  
+    // Checks if both email and password fields are filled
+    if (!email || !password) {
+      setLoginMessage("Please fill in all input slots");
+      return;
     }
-
+  
     try {
-      // Handle incomplete data
-      if (!loginData.email || !loginData.password) throw {requestMessage: "Please fill in all input slots"}
-      const response = await axiosProvider.post(
-        "/login",
-        loginData,
-        postOptions
-      )
-
-      localStorage.setItem('token', response.data.accessToken); // Store the token
+      // Sends a POST request to the login endpoint with email and password
+      const response = await axiosProvider.post("/login", { email, password });
+  
+      // Stores the token and user data in localStorage upon successful login
+      localStorage.setItem('token', response.data.accessToken);
       const userData = {
         uuid: response.data.user.uuid,
         name: response.data.user.name,
         email: response.data.user.email
       };
-      localStorage.setItem('user', JSON.stringify(userData))
-
-      setMessage("Login successful!");
-      navigate("/")
-    }
-    catch(error){
-      const errorMessage = error.requestMessage || error.response?.data?.message || 'Login failed, please try again.';
-      setMessage(errorMessage);
-    }
-  }
+      localStorage.setItem('user', JSON.stringify(userData));
   
-  // if user click guest visit redirect to main map
+      setLoginMessage("Login successful!"); // Sets a success message
+      navigate("/"); // Navigates to the home page or dashboard
+    } catch (error) {
+      // Sets an error message if the login process fails
+      setLoginMessage(error.message || 'Login failed, please try again.');
+    }
+  };
+  
+
+  const handleForgotPassword = async (evt) => {
+    const email = popupFormRef.current?.email?.value;
+    if (!email) {
+      evt.preventDefault()
+      setPopupMessage("Please enter your email.");
+      return;
+    }
+    try {
+      await axiosProvider.post("/forgotpassword", { email });
+      setPopupMessage("Reset password link sent to your email.");
+    } catch (error) {
+      setPopupMessage(error.message || 'Error sending reset password link.');
+    }
+  };
+
+  const forgotPasswordData = {
+    title: "Forgot Password",
+    inputs: [{id:"email", name:"email", type:"email", placeholder:"Enter your email"}],
+    buttons: [
+      {value: "Discard", handleClick: closePopup},
+      {value: "Submit", handleClick: handleForgotPassword}
+    ]
+  };
+
   const handleGuest = (evt) => {
-    evt.preventDefault()
-    // pop up alert: confirm guest visit
-    const continueGuest  = window.confirm('Guest visit will not save your data, continue?');
+    evt.preventDefault();
+    const continueGuest = window.confirm('Guest visit will not save your data, continue?');
     if (continueGuest) {
-      navigate("/", { state: { from: location.pathname } });
+      navigate("/");
     }
-  }
-  
+  };
 
-  return(
+
+  const handlePopupFormSubmit = (evt) => {
+    evt.preventDefault();
+    if (currentActionData && currentActionData.buttons[0]) {
+      currentActionData.buttons[0].handleClick();
+    }
+  };
+
+  return (
     <>
-      <AuthHeader header="Login" message={message}/>
-      <form ref={formRef} onSubmit = {handleClick}>
+      <AuthHeader header="Login" message={loginMessage} />
+      <form ref={formRef} onSubmit={handleLogin}>
         <FormInputs fields={fields} />
         <div className='mt-2'>
-          <FormBtn type="submit" handleClick={handleClick}/>
-          <FormBtn value="Guest Visit" handleClick={handleGuest}/>
+          <FormBtn type="submit" value="Login" />
+          <FormBtn value="Guest Visit" handleClick={handleGuest} />
         </div>
       </form>
       <div className='mt-2'>
-        <PageLink to="/register" value="Register"/>
-        <PageLink to="/login" value="Forget Password"/>
+        <PageLink to="/register" value="Register" />
+        <div className='w-full text-center py-2 underline'>
+          <span onClick={() => setCurrentActionData(forgotPasswordData)} style={{ cursor: 'pointer' }}>
+            Forgot Password
+          </span>
+        </div>
+        {currentActionData &&
+          <form ref={popupFormRef} onSubmit={handlePopupFormSubmit} onClick={(e) => e.stopPropagation()}>
+            <PopupContent 
+              message={popupMessage}
+              title={currentActionData.title}
+              inputs={currentActionData.inputs}
+              buttons={currentActionData.buttons}
+              handleClick={closePopup}
+            />
+          </form>
+        }
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
