3c3c50652c363a7ba8c75342f3feae552ec948b3
Nina1o1 <qiwenxiao16@gmail.com>
2023-12-12 16:29:31 -0500

    minor change on register page

--- front-end/src/pages/Authenticate/Register.jsx
+++ front-end/src/pages/Authenticate/Register.jsx
@@ -8,14 +8,16 @@ import FormBtn from '../../components/form/formBtn';
 
 const Register = () => {
   const [message, setMessage] = useState('')
-  const fields = ['username', 'email', 'password']
   const navigate = useNavigate()
+
+  const fields = ['username', 'email', 'password']
+  
   const handleClick = async (evt) => {
     evt.preventDefault();
-
     
     try {
       const formData = {};
+
       fields.forEach((field) => {
         const inputData = document.getElementById(field).value
         // Handle incomplete data
@@ -23,9 +25,10 @@ const Register = () => {
         formData[field] = inputData
       });
 
-      const response = await axiosProvider.post("/register", formData);
-      setMessage(response.data.message)
+      await axiosProvider.post("/register", formData);
+      setMessage("")
       navigate("/login")
+      
     } catch (error) {
       const errorMessage = error.requestMessage || error.response?.data?.message || 'Login failed, please try again.';
       setMessage(errorMessage)
@@ -53,12 +56,14 @@ const Register = () => {
         `}
        </style>
       <AuthHeader header="Register" message={message} />
+      
       <form>
         <FormInputs fields={fields} />
         <div className='mt-2'>
           <FormBtn handleClick={handleClick} />
         </div>
       </form>
+
       <div className='mt-2'>
         <PageLink to="/login" value="Login" />
       </div>
